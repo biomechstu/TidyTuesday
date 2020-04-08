32049b383ec1ca052e3e6b5a950a1026aa32a8ee
Stuart McErlain-Naylor <s.mcerlain-naylor@uos.ac.uk>
2020-04-08 10:57:57 +0100

    Create TidyTuesdayTdF_v1.R

---
+++ 07_04_20/TidyTuesdayTdF_v1.R
@@ -0,0 +1,35 @@
+# Load libraries
+library(tidyverse)
+library(kableExtra)
+library(magick)
+
+# Read data
+tdf_winners <- read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-04-07/tdf_winners.csv')
+
+for(i in 1:106) {
+tdf_winners %>%
+  filter(edition <=i) %>%
+  count(winner_name, name = 'num_of_wins') %>%
+  mutate(rank = rank(-num_of_wins)) %>%
+  filter(rank <=20) %>%
+  #filter(num_of_wins > 1) %>%
+  ggplot(aes(reorder(winner_name, num_of_wins), num_of_wins),main=edition) +
+  geom_col(fill = "blue",
+           width = 0.6,
+           alpha = 0.8) +
+  labs(x = NULL, y = paste("Tour de France wins - ",i,sep="")) +
+  coord_flip()
+  if (i<10) {
+    ggsave(filename=paste("00",i,".png",sep=""))
+  } else if ( i<100) {
+    ggsave(filename=paste("0",i,".png",sep=""))
+  } else {
+    ggsave(filename=paste(i,".png",sep=""))
+  }
+}
+
+list.files(pattern = "*.png") %>% 
+  map(image_read) %>% # reads each path file
+  image_join() %>% # joins image
+  image_animate(fps=2) %>% # animates, can opt for number of loops
+  image_write("TdF.gif") # write to current dir
